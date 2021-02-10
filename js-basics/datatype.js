fc0347cfccfb18fe16ecfc282890bdd0b767f9f3
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2021-02-10 10:52:11 +0530

    Add files via upload

---
+++ js-basics/datatype.js
@@ -0,0 +1,28 @@
+/*function a (){
+    return 1
+}*/
+let a = function(){
+    return 1
+}
+
+let b = 1
+
+
+console.log(13313,typeof 13313)
+console.log(234234.5356,typeof 234234.5356)
+console.log('a',typeof 'a')
+console.log("asdsdsdfsdsa", typeof "asdsdsdfsdsa ")
+console.log(true,typeof true)
+console.log(null,typeof null)
+console.log(undefined, typeof undefined)
+console.log(a,typeof a)
+console.log(a)
+console.log(a())
+console.log(a(),typeof a())
+
+a()
+console.log(b())
+
+console.log((a),(b))
+console.log(a,b)
+console.log(a(),b())
