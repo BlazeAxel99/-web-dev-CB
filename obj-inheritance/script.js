fa87440a3331f8e3d23c5500832fe08cb774eeff
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2021-02-17 13:04:38 +0530

    Add files via upload

---
+++ obj-inheritance/script.js
@@ -0,0 +1,24 @@
+let obj1= {
+    a:10,
+    b:20,
+    c:30
+}
+
+let obj2 = Object.create(obj1)
+obj2.p = 'adg'
+obj2.q = 'dgn'
+obj2.r = 'dfn'
+//obj3.__proto__=obj2
+//obj3.__proto__.__proto__=obj1
+let obj3 = Object.create(obj2)
+/*
+  obj2.a -> it will try to find a in obj2
+         -> if not found,
+         -> it will try to find a in obj2.__proto__
+         -> if not found,
+         -> it will try to find a in obj2.__proto__.__proto__
+         ->...and so on ...
+         -> till __ proto__ becomes null
+
+
+*/
