9f720df521f625bd650277a41779b6c611281e4c
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2021-02-17 12:05:59 +0530

    Add files via upload

---
+++ string-literals/script.js
@@ -0,0 +1,23 @@
+//he said,"This is awesome!"
+
+let s1= 'he said, "This is awesome!"'
+
+let s2= "he said, \"This is awesome!\""
+
+let s3= "he said, 'This is awesome!'"
+
+let s4 = `he said, 'This is awesome!'`
+
+let s5 = "this string is \n in multiple lines"
+
+let s6 = `
+this string is
+in multiple lines
+`
+
+let name = 'John'
+
+let g1 = 'Good Morning ' + name
+
+let g2 = `Good Morning ${name}` 
+let s7 = `10 + 2 = ${10 + 2}`
